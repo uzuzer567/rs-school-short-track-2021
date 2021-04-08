4b157658631bd947256701c321b06957c40d05b4
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-04-08 13:49:36 +0300

    feat: add function

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -9,7 +9,24 @@
  *
  */
 function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+  let emailArray = email.split('');
+  let answerArray = [];
+  let answer = "";
+  let counter = 0;
+  for(let i = emailArray.length; i > 0; i--) {
+	  if(counter === 0) {
+		 if(emailArray[i] !== "@") {
+		   answerArray.push(emailArray[i]);
+		 }
+		 else {
+			 counter++;
+		 }
+	  }
+  }
+  for(let i = 0; i < answerArray.length; i++) {
+	  answer += answerArray[i];
+  }
+  return answer;
 }
 
 module.exports = getEmailDomain;
