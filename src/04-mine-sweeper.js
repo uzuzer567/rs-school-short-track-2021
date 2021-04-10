02c07466529bcc28d2323d5af684f8c56df718c6
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-04-10 12:47:51 +0300

    feat: add function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,68 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  let trueCounter = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] !== false) {
+        trueCounter++;
+      }
+    }
+  }
+
+  const answerMatrix = new Array(matrix.length);
+  for (let i = 0; i < matrix.length; i++) {
+    answerMatrix[i] = new Array(matrix[0].length);
+  }
+
+  if (trueCounter === 0) {
+    for (let i = 0; i < answerMatrix.length; i++) {
+      for (let j = 0; j < answerMatrix[0].length; j++) {
+        answerMatrix[i][j] = 0;
+      }
+    }
+  } else {
+    for (let i = 0; i < answerMatrix.length; i++) {
+      for (let j = 0; j < answerMatrix[0].length; j++) {
+        answerMatrix[i][j] = 0;
+      }
+    }
+    for (let i = 0; i < answerMatrix.length; i++) {
+      for (let j = 0; j < answerMatrix[0].length; j++) {
+        if (i !== matrix.length - 1 && matrix[i + 1][j] === true) {
+          answerMatrix[i][j] += 1;
+        }
+        if (j !== matrix[0].length - 1 && matrix[i][j + 1] === true) {
+          answerMatrix[i][j] += 1;
+        }
+        if (i !== 0 && matrix[i - 1][j] === true) {
+          answerMatrix[i][j] += 1;
+        }
+        if (j !== 0 && matrix[i][j - 1] === true) {
+          answerMatrix[i][j] += 1;
+        }
+        if (i !== 0 && j !== matrix[0].length - 1 && matrix[i - 1][j + 1] === true) {
+          answerMatrix[i][j] += 1;
+        }
+        if (j !== 0 && i !== matrix.length - 1 && matrix[i + 1][j - 1] === true) {
+          answerMatrix[i][j] += 1;
+        }
+        if (i !== 0 && j !== 0 && matrix[i - 1][j - 1] === true) {
+          answerMatrix[i][j] += 1;
+        }
+      }
+    }
+    for (let i = answerMatrix.length - 1; i > 0; i--) {
+      for (let j = answerMatrix[0].length - 1; j > 0; j--) {
+        if (i !== 0 && j !== 0 && matrix[i][j] === true) {
+          answerMatrix[i - 1][j - 1] += 1;
+        }
+      }
+    }
+  }
+
+  return answerMatrix;
 }
 
 module.exports = minesweeper;
