9238335e8282d3fe517a47249cd7e4e6c8d50d68
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-04-11 16:00:21 +0300

    feat: add function

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const strArray = str.split('');
+  let counter = 1;
+  let answer = '';
+  for (let i = 0; i < strArray.length; i++) {
+    if (strArray[i] === strArray[i + 1]) {
+      counter++;
+    }
+    if (strArray[i] !== strArray[i + 1]) {
+      if (counter !== 1) {
+        answer += counter;
+        answer += strArray[i];
+        counter = 1;
+      } else {
+        answer += strArray[i];
+        counter = 1;
+      }
+    }
+  }
+  return answer;
 }
 
 module.exports = encodeLine;
